de84810e37a82a4c3d89ef3452cd526ab2080c86
Lance Watt <lanceawatt@hotmail.com>
2021-12-26 12:07:39 +0100

    initial commit

---
+++ CreateProjectTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CreateProjectTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
